070e0fc1226cb43c0f0414eeb0e6992f0f907fca
Chris Galbraith <chris@resknow.co.uk>
2019-08-23 15:51:01 +0100

    first commit

---
+++ homebrew-boilerplate-cli.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class BoilerplateCli < Formula
+  desc "A super simple CLI for installing Boilerplate"
+  homepage ""
+  url "https://github.com/resknow/boilerplate-cli/archive/1.0.0.tar.gz"
+  sha256 "a1b16231f67d1a298bed718a9795cc9f46fa481e14bbf3a589ce44e296634141"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test boilerplate-cli`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
